daf3a6ea8b6faaabe1077b63638b5ee587f4792e
AnselmoJimenez <anselmojimenez0@gmail.com>
2023-08-21 10:05:27 -0400

    TODO: Lumination dot product

--- cube.c
+++ cube.c
@@ -1,37 +1,64 @@
 #include "cube.h"
 
 void render_frame(void) {
-    float z_buffer[SCREEN_HEIGHT][SCREEN_WIDTH] = { [0 ... SCREEN_HEIGHT - 1][0 ... SCREEN_WIDTH - 1] = 0.0f };
-    char out_buffer[SCREEN_HEIGHT][SCREEN_WIDTH] = { [0 ... SCREEN_HEIGHT - 1][0 ... SCREEN_WIDTH - 1] = ' ' };
+    float A = 0;
 
-    // X coordinate
-    for (float x = -WIDTH; x < WIDTH; x += 0.05) {
-        // Y coordinate
-        for (float y = -HEIGHT; y < HEIGHT + 1; y += 0.05) {
-            // distance from the screen
-            float z = 30.0f;
-            float z_inv = 1.0f / z;
+    float cosA = cos(A);
+    float sinA = sin(A);
 
-            // projections
-            int x_projection = (int) (SCREEN_WIDTH / 2) + x * FOV * z_inv;
-            int y_projection = (int) (SCREEN_HEIGHT / 2) - y * FOV * z_inv;
+    float z_buffer[SCREEN_HEIGHT * SCREEN_WIDTH] = { [0 ... SCREEN_HEIGHT * SCREEN_WIDTH - 1] = 0.0f };
+    char out_buffer[SCREEN_HEIGHT * SCREEN_WIDTH] = { [0 ... SCREEN_HEIGHT * SCREEN_WIDTH - 1] = ' ' };
 
+    for (;;) {
+        // reset contents of buffers
+        memset(z_buffer, 0.0f, sizeof(z_buffer));
+        memset(out_buffer, ' ', sizeof(out_buffer));
 
-            out_buffer[y_projection][x_projection] = '#';
+        // X coordinate
+        for (float x = -WIDTH; x < WIDTH; x += SPEED) {
+            // Y coordinate
+            for (float y = -HEIGHT; y < HEIGHT; y += SPEED) {
+                // Z coordinate
+                for (float z = -DEPTH; z < DEPTH; z += SPEED) {
+                    float preeval = x * cosA + y * sinA;
+
+                    // rotation calculations
+                    float rx = -z * sinA + cosA * preeval;
+                    float ry = y * cosA - x * sinA;
+                    float rz = (z * cosA + sinA * preeval) + CAMERA_DISTANCE;
+                    float z_inv = 1.0f / rz;
+
+                    // projections
+                    int x_projection = (int) (SCREEN_WIDTH / 2) + rx * FOV * z_inv;
+                    int y_projection = (int) (SCREEN_HEIGHT / 2) - ry * FOV * z_inv;
+
+                    int index = x_projection + y_projection * SCREEN_WIDTH;
+                    if (z_inv > z_buffer[index]) {
+                        z_buffer[index] = z_inv;
+                        out_buffer[index] = '#';
+                    }   
+                }
+            }
         }
-    }
-
-    // print output
-    for (int j = 0; j < SCREEN_HEIGHT; j++) {
-        for (int i = 0; i < SCREEN_WIDTH; i++) {
-            printf("%c", out_buffer[i][j]);
+        
+        printf("\x1b[H");   // move cursor to top left
+        
+        // print output
+        for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; i++) {
+            if (i % SCREEN_WIDTH == 0) putchar('\n');
+            putchar((int) out_buffer[i]);
         }
-        printf("\n");
-    }
+        putchar('\n');
 
+        A += 0.05;
+
+        cosA = cos(A);
+        sinA = sin(A);
+    }
 }
 
 int main(void) {
+    printf("\x1b[2J");  // Clear screen
     render_frame();
 
     return 0;
--- cube.h
+++ cube.h
@@ -2,18 +2,21 @@
 #define CUBE_H_
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <math.h>
 
 #define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 128
+#define SCREEN_HEIGHT 44
 
 #define FOV 50.0f
 
-#define HEIGHT 5
-#define WIDTH 5
-#define DEPTH 5
+#define SPEED 0.2f
+#define CAMERA_DISTANCE 100
+
+#define HEIGHT 20.0f
+#define WIDTH 20.0f
+#define DEPTH 20.0f
 
 // Render a frame of the donut
 void render_frame(void);
