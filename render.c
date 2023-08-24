c7ab1122081c619fe9308e4750420dc5451468bf
AnselmoJimenez <anselmojimenez0@gmail.com>
2023-08-24 10:29:26 -0400

    turned into main file to handle all screen rendering

---
+++ render.c
@@ -0,0 +1,39 @@
+#include "render.h"
+#include "cube.h"
+
+static float z[SCREEN_HEIGHT * SCREEN_WIDTH] = { [0 ... SCREEN_HEIGHT * SCREEN_WIDTH - 1] = 0.0f };
+static char out[SCREEN_HEIGHT * SCREEN_WIDTH] = { [0 ... SCREEN_HEIGHT * SCREEN_WIDTH - 1] = ' ' };
+
+
+void render() {
+    // clear screen
+    printf("\x1b[2J");
+
+    cube_t *cube = (cube_t *) calloc(1, sizeof(cube_t));
+    cube->width = 20.0f;
+    cube->height = 20.0f;
+    cube->depth = 20.0f;
+
+    while (true) {
+        // reset coordinate buffer and output buffer
+        memset(z, 0.0f, sizeof(z));
+        memset(out, '.', SCREEN_HEIGHT * SCREEN_WIDTH);
+        
+        // render
+        render_cube_frame(z, out, cube);
+
+        // move cursor to top left of screen and print output
+        printf("\x1b[H");
+        for (int i = 0; i < SCREEN_HEIGHT * SCREEN_WIDTH; i++) {
+            if (i % SCREEN_WIDTH == 0) putchar('\n');
+            putchar((int) out[i]);
+        }
+        putchar('\n');
+
+    }
+}
+
+int main(void) {
+    render(z, out);
+    return 0;
+}
---
+++ render.h
@@ -0,0 +1,18 @@
+#ifndef RENDER_H_
+#define RENDER_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+
+#define SCREEN_WIDTH 156
+#define SCREEN_HEIGHT 48
+
+#define FOV 50.0f
+
+#define OBJECT_DISTANCE 100.0f
+#define STRETCH 2
+
+#endif
