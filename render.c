6638a98089c72ca854f43cdd750d16f19dfee329
AnselmoJimenez <anselmojimenez0@gmail.com>
2023-08-30 01:09:09 -0400

    implemented terminal arguments for selecting shape

--- render.c
+++ render.c
@@ -10,9 +10,11 @@ static float z[SCREEN_HEIGHT * SCREEN_WIDTH] = { [0 ... SCREEN_HEIGHT * SCREEN_W
 static char out[SCREEN_HEIGHT * SCREEN_WIDTH] = { [0 ... SCREEN_HEIGHT * SCREEN_WIDTH - 1] = ' ' };
 
 
-void render(shape_t shape) {
+void render(shape_t shape, axis_t axis) {
     // clear screen
     printf("\x1b[2J");
+
+    // TODO: implement user input rotation handling
 
     /* cube ****************************************************************/
     cube_t *cube = (cube_t *) calloc(1, sizeof(cube_t));
@@ -73,6 +75,20 @@ void render(shape_t shape) {
         memset(out, ' ', SCREEN_HEIGHT * SCREEN_WIDTH);
         
         // render
+        switch (shape) {
+            case CUBE:
+                render_cube_frame(cube, z, out);
+                break;
+            case SPHERE:
+                render_sphere_frame(sphere, z, out);
+                break;
+            case CONE:
+                render_cone_frame(cone, z, out);
+                break;
+            case CYLINDER:
+                render_cylinder_frame(cylinder, z, out);
+                break;
+        }
         // render_cube_frame(cube, z, out);
         // render_cone_frame(cone, z, out);
         // render_cylinder_frame(cylinder, z, out);
@@ -88,18 +104,62 @@ void render(shape_t shape) {
     }
 }
 
-int main(int argc, char *argv[]) {
-    int opt;
+// take shape argument from terminal and convert to enum
+shape_t enumify(const char *shape) {
+    char temp[strlen(shape) + 1];
 
+    // convert argument to uppercase for comparison
+    int i = 0;
+    while (shape[i] != '\0') {
+        temp[i] = toupper(shape[i]);
+        i++;
+    }
+    temp[i] = '\0';
+
+    // return enum of option
+    if (strcmp(temp, "CUBE") == 0) return CUBE;
+    else if (strcmp(temp, "SPHERE") == 0) return SPHERE;
+    else if (strcmp(temp, "CONE") == 0) return CONE;
+    else if (strcmp(temp, "CYLINDER") == 0) return CYLINDER;
+    
+    // undefined shape
+    return -1;
+}
+
+int main(int argc, char *argv[]) {
+    const char *sarg = NULL;
+    shape_t shape = 0;
+
+    // read the terminal arguments
+    int opt;
     while((opt = getopt(argc, argv, ARGUMENTS)) != -1) {
         switch(opt) {
+            case 'h':
+                printf(USAGE);
+                return 0;
             case 's':
-                printf("option: %s\n", optarg);
+                sarg = optarg;
                 break;
+            default:
+                fprintf(stderr, "Unknown command line option, aborting...\n");
+                return -1;
         }
     }
+    
+    // No arguments
+    if (sarg == NULL) {
+        fprintf(stderr, "no arguments specified, aborting...\n");
+        return -1;
+    }
 
-    // render(z, out);
+    // turn the shape input into shape_t type to be able to be rendered
+    shape = enumify(sarg);
+    if (shape == -1) {
+        fprintf(stderr, "invalid shape option, aborting...\n");
+        return -1;
+    }
+
+    render(shape, X);
 
     return 0;
 }
