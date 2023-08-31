d174f4b7723133196ca165d2aeb38db088792e16
AnselmoJimenez <anselmojimenez0@gmail.com>
2023-08-31 13:57:44 -0400

    changed rotations to XYZ axis

--- cube.c
+++ cube.c
@@ -6,10 +6,10 @@
 char determine_cube_char(cube_t *cube, int cx, int cy, int cz) {
     if      (cz == -cube->depth)    return '@'; // front face
     else if (cy == -cube->height)   return '%'; // top face
-    else if (cx == -cube->width)    return '$'; // right face
-    else if (cz == cube->depth)     return '*'; // back face
+    else if (cx == -cube->width)    return '+'; // right face
+    else if (cz == cube->depth)     return '='; // back face
     else if (cy == cube->height)    return '#'; // bottom face
-    else if (cx == cube->width)     return '?'; // left face
+    else if (cx == cube->width)     return '/'; // left face
 
     // default
     return '.';
@@ -27,9 +27,9 @@ void render_cube_frame(cube_t *cube, float *z, char *out) {
             // X Coordinate
             for (float cx = -cube->width; cx <= cube->width; cx += COORD_INCREMENT) {
                 // rotation calculations for coordinates
-                float rx = cx*cosA + cy*sinA;
-                float ry = cz*sinA + cy*cosA*cosA - cx*sinA*cosA;
-                float rz = cz*cosA - cy*cosA*sinA - cx*sinA*sinA + OBJECT_DISTANCE;
+                float rx = cx*cosA*cosA + cy*cosA*sinA - cz*cosA*sinA + cy*cosA*sinA*sinA - cx*sinA*sinA*sinA;
+                float ry = cz*sinA + cy*cosA*cosA - cx*cosA*sinA;
+                float rz = cx*cosA*sinA + cy*sinA*sinA + cz*cosA*cosA - cy*cosA*cosA*sinA + cx*cosA*sinA*sinA + OBJECT_DISTANCE;
 
                 float z_inv = 1.0f / rz;
 
