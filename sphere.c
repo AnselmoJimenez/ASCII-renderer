fa55e401399cb3ce2f6f082ab2633475d5fc561e
AnselmoJimenez <anselmojimenez0@gmail.com>
2023-09-07 16:53:19 -0400

    redid illumination calculations

--- sphere.c
+++ sphere.c
@@ -2,11 +2,11 @@
 #include "sphere.h"
 
 // determine the character to display based off of the lumination
-char determine_sphere_lumination(sphere_t *sphere, float sx, float sy, float sz) {
+char illuminate(sphere_t *sphere, float sx, float sy, float sz) {
     // lighting direction
-    int lx = 0;
-    int ly = 1;
-    int lz = -1;
+    float lx = 0.0f;
+    float ly = -0.707f;
+    float lz = 0.707f;
     
     // surface normal vector
     float nx = sx / sphere->r;
@@ -14,30 +14,40 @@ char determine_sphere_lumination(sphere_t *sphere, float sx, float sy, float sz)
     float nz = sz / sphere->r;
 
     // lumination intensity based on surface normal and lighting direction
-    // negated for indexing purposes
-    int luminance = (int) -(nx*lx + ny*ly + nz*lz);
-    return "0$#?!/:+~,."[luminance];
+    // negated for because what is shown is the sphere
+    float luminance = (nx*lx + ny*ly + nz*lz) * 8;
+    if (luminance < 0) luminance *= -1;
+    return ".,:;=#$@"[(int) luminance];
 }
 
 // Render a frame of the sphere
 void render_sphere_frame(sphere_t *sphere, float *z, char *out) {
+    // Preevaluate sin and cos of angles
+    float cosA = cos(angle), sinA = sin(angle);
+
     // Theta angle between point at sphere surface and z plane
-    for (float theta = 0.0f; theta < PI; theta += ANGLE_INCREMENT) {
+    for (float theta = 0.0f; theta < PI; theta += THETA_INCREMENT) {
         float cosT = cos(theta), sinT = sin(theta);
         // Phi angle between xy plane and positive x axis
-        for (float phi = 0.0f; phi < 2 * PI; phi += ANGLE_INCREMENT) {
+        for (float phi = 0.0f; phi < 2 * PI; phi += PHI_INCREMENT) {
             float cosP = cos(phi), sinP = sin(phi);
 
             // calculating surface coordinates
             float sx = sphere->r * sinT * cosP;
             float sy = sphere->r * sinT * sinP;
-            float sz = sphere->r * cosT + OBJECT_DISTANCE;
+            float sz = sphere->r * cosT;
 
-            float z_inv = 1.0f / sz;
+            // rotating surface coordinates
+            // XYZ rotations
+            float rx = sx*cosA*cosA + sy*cosA*sinA - sz*cosA*sinA + sy*cosA*sinA*sinA - sx*sinA*sinA*sinA;
+            float ry = sz*sinA + sy*cosA*cosA - sx*cosA*sinA;
+            float rz = sx*cosA*sinA + sy*sinA*sinA + sz*cosA*cosA - sy*cosA*cosA*sinA + sx*cosA*sinA*sinA + OBJECT_DISTANCE;
+
+            float z_inv = 1.0f / rz;
 
             // calculating projection onto screen
-            int x_projection = (int) (SCREEN_WIDTH / 2) + sx * FOV * z_inv * STRETCH;
-            int y_projection = (int) (SCREEN_HEIGHT / 2) - sy * FOV * z_inv;
+            int x_projection = (int) (SCREEN_WIDTH / 2) + rx * FOV * z_inv * STRETCH;
+            int y_projection = (int) (SCREEN_HEIGHT / 2) - ry * FOV * z_inv;
 
             // Get the index of the projection on the screen
             int index = x_projection + y_projection * SCREEN_WIDTH;
@@ -46,7 +56,7 @@ void render_sphere_frame(sphere_t *sphere, float *z, char *out) {
             if (-1 < index && index < SCREEN_HEIGHT * SCREEN_WIDTH) {
                 if (z_inv > z[index]) {
                     z[index] = z_inv;
-                    out[index] = determine_sphere_lumination(sphere, sx, sy, sz);
+                    out[index] = illuminate(sphere, sx, sy, sz);
                 }
             }
         }
--- sphere.h
+++ sphere.h
@@ -1,12 +1,15 @@
 #ifndef SPHERE_H_
 #define SPHERE_H_
 
+#define THETA_INCREMENT 0.02f
+#define PHI_INCREMENT 0.02f
+
 typedef struct {
     int r;      // radius of sphere
 } sphere_t;
 
 // determine the character to display based off of the lumination
-char determine_sphere_lumination(sphere_t *sphere, float sx, float sy, float sz);
+char illuminate(sphere_t *sphere, float sx, float sy, float sz);
 
 // Render a frame of the sphere
 void render_sphere_frame(sphere_t *sphere, float *z, char *out);
